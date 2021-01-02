ad82f364c86b77237a10299be345d4f99d0b3935
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2021-01-02 11:53:32 +0100

    Add empty a star file

---
+++ backend/routeplanner/planner/mapprovider/a_star.py
@@ -0,0 +1,9 @@
+import osmnx as ox
+import networkx as nx
+from .map import get_graph
+
+def find_path(node1, node2):
+    # TODO: Implement a star algorithm here, function should return ordered list of nodes in path
+    # The path should not include first node
+    # graph = get_graph()
+    return []
