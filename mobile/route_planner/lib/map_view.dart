7384e6baa34368fe4c36e9203c449de6478adea4
Rolfrider <ravkwiatkowski@gmail.com>
2021-01-08 00:13:45 +0100

    Add MapView

---
+++ mobile/route_planner/lib/map_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  CameraPosition _initialLocation = CameraPosition(target: LatLng(52.2, 24.1));
+  GoogleMapController mapController;
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    return Container(
+      height: height,
+      width: width,
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              initialCameraPosition: _initialLocation,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              mapType: MapType.normal,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: false,
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _moveToPosition(LatLng pos) {
+    mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(
+            // Will be fetching in the next step
+            pos.latitude,
+            pos.longitude,
+          ),
+          zoom: 18.0,
+        ),
+      ),
+    );
+  }
+}
