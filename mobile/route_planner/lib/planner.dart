e709b4cd116e4b6999acaf06cc8dd5d6d031162e
Rolfrider <ravkwiatkowski@gmail.com>
2021-01-08 17:11:49 +0100

    Move url to const

--- mobile/route_planner/lib/planner.dart
+++ mobile/route_planner/lib/planner.dart
@@ -3,9 +3,10 @@ import 'dart:convert';
 
 import 'model/point.dart';
 
+const _url = "http://10.0.2.2:8000/planner/";
+
 Future<List<Point>> routeFor(List<Point> points) async {
-  final request =
-      http.Request('GET', Uri.parse("http://10.0.2.2:8000/planner/"));
+  final request = http.Request('GET', Uri.parse(_url));
   request.body = jsonEncode(points);
   return request
       .send()
